ed09e42236afb9c0cbe7c4841110e5ffe5c8c565
Andrea <andreacolucci1994@gmail.com>
2022-08-26 15:40:12 +0200

    Exercise

---
+++ src/main/java/co/develhope/MockExercise/MockExerciseApplication.java
@@ -0,0 +1,14 @@
+package co.develhope.MockExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class MockExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockExerciseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/develhope/MockExercise/entities/Student.java
@@ -0,0 +1,22 @@
+package co.develhope.MockExercise.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+}
